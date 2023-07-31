605e3096c308e191f657c8ac9599cf14f672281a
sanjana awachat <you@example.com>
2023-07-31 08:21:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('this is ecommerce project');
+function fun1() {
+  document.getElementById('demo').innerHTML = 'Thanku For Payment !';
+}
+function fun2() {
+  document.getElementById('explore').innerHTML = 'Thanku For Exploring!';
+}
